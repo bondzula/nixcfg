4578098201967387f7ecc9a8da6d0d54a57fb4ad
Stefan Bondzulic <stefanbondzulic@gmail.com>
2025-07-03 18:26:00 +0200

    feat(c): improve which package we are using based on the system

--- modules/homeManager/dev/c.nix
+++ modules/homeManager/dev/c.nix
@@ -4,21 +4,35 @@
   pkgs,
   ...
 }:
+let
+  useGcc = config.homeModules.dev.c.preferGcc;
 
+  compiler = if useGcc then pkgs.gcc else pkgs.clang;
+  debugger = if useGcc then pkgs.gdb else pkgs.lldb;
+in
 {
-  options.homeModules.dev.c.enable = lib.mkEnableOption "Enable C / C++ Module";
+  options.homeModules.dev.c = {
+    enable = lib.mkEnableOption "Enable C / C++ Module";
+    preferGcc = lib.mkOption {
+      type = lib.types.bool;
+      default = pkgs.stdenv.isDarwin;
+      description = "Whether to prefer GCC over Clang";
+    };
+  };
 
   config = lib.mkIf config.homeModules.dev.c.enable {
-    home.packages = with pkgs; [
-      clang
-      gcc
-      cmake
-      gnumake
-      lldb
-      gdb
-      clang-tools
-      pkg-config
-      cmake-language-server
-    ];
+    home.packages =
+      with pkgs;
+      [
+        cmake
+        gnumake
+        clang-tools # Works well on both systems for LSP/formatting
+        pkg-config
+        cmake-language-server
+      ]
+      ++ [
+        compiler
+        debugger
+      ];
   };
 }
