ccddf4aef81973d4fc173fa9877f8be18e7c5885
Stefan Bondzulic <stefanbondzulic@gmail.com>
2025-08-05 20:06:31 +0200

    fix(corrino): use as standalone config

--- hosts/corrino/home.nix
+++ hosts/corrino/home.nix
@@ -1,64 +1,51 @@
 {
   pkgs,
-  inputs,
-  outputs,
   ...
 }:
 
 {
   imports = [
-    inputs.home-manager.nixosModules.home-manager
+    ../../modules/homeManager
   ];
 
-  home-manager = {
-    useUserPackages = true;
-    extraSpecialArgs = { inherit inputs outputs; };
-    users.bondzula = {
-      imports = [
-        ../../modules/homeManager
-      ];
+  home = {
+    username = "bondzula";
+    homeDirectory = "/home/bondzula";
 
-      home = {
-        username = "bondzula";
-        homeDirectory = "/home/bondzula";
+    packages = with pkgs; [
 
-        packages = with pkgs; [
-          
-        ];
+    ];
 
-        stateVersion = "24.11";
-      };
-
-      homeModules = {
-        cli = {
-          atuin.enable = true;
-          direnv.enable = true;
-          fzf.enable = true;
-          git.enable = true;
-          neovim.enable = true;
-          ripgrep.enable = true;
-          zoxide.enable = true;
-          zsh.enable = true;
-        };
-
-        dev = {
-          aws.enable = true;
-          c.enable = true;
-          go.enable = true;
-          javascript.enable = true;
-          lua.enable = true;
-          nix.enable = true;
-          php.enable = true;
-          python.enable = true;
-          rust.enable = true;
-          terraform.enable = true;
-          zig.enable = true;
-        };
-
-      };
-
-      programs.home-manager.enable = true;
-    };
+    stateVersion = "24.11";
   };
 
+  homeModules = {
+    cli = {
+      atuin.enable = true;
+      direnv.enable = true;
+      fzf.enable = true;
+      git.enable = true;
+      neovim.enable = true;
+      ripgrep.enable = true;
+      zoxide.enable = true;
+      zsh.enable = true;
+    };
+
+    dev = {
+      aws.enable = true;
+      c.enable = true;
+      go.enable = true;
+      javascript.enable = true;
+      lua.enable = true;
+      nix.enable = true;
+      php.enable = true;
+      python.enable = true;
+      rust.enable = true;
+      terraform.enable = true;
+      zig.enable = true;
+    };
+
+  };
+
+  programs.home-manager.enable = true;
 }
