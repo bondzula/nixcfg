c0c640e82cc0dc8ed26569cbaed1779a3e780cc2
Stefan Bondzulic <stefanbondzulic@gmail.com>
2025-10-02 12:42:49 +0200

    feat: add new media host

--- flake.nix
+++ flake.nix
@@ -51,6 +51,13 @@
           ];
         };
 
+        media = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/media
+          ];
+        };
+
 
         # Old Hosts
         jakku = nixpkgs.lib.nixosSystem {
--- hosts/media/default.nix
+++ hosts/media/default.nix
@@ -2,11 +2,11 @@
 
 {
   imports = [
-    (modulesPath + "/virtualisation/proxmox-lxc.nix")
+    (modulesPath + "/virtualisation/proxmox-lxc.nix") # Substitute for hardware-config.nix when working with proxmox LXC
     ../../modules/nixos
   ];
 
-  networking.hostName = "proxy";
+  networking.hostName = "media";
 
   nix.settings.sandbox = false;
   nixpkgs.hostPlatform = "x86_64-linux";
@@ -16,6 +16,23 @@
     privileged = false;
   };
 
+  ## Arc drivers & VA-API
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # iHD: mandatory for Arc
+      intel-compute-runtime # OpenCL: HDR tone-mapping & subtitles
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-ocl # OpenCL support
+    ];
+  };
+
+  ## environment tweaks
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+    LIBVA_DRIVERS_PATH = "${pkgs.intel-media-driver}/lib/dri";
+  };
+
   time.timeZone = "Europe/Belgrade";
   i18n.defaultLocale = "en_US.UTF-8";
 
@@ -23,16 +40,23 @@
     enable = true;
   };
 
+  # Setup users
   users = {
     users.bondzula = {
       initialHashedPassword = "$y$j9T$lTYSuKE.0BiJazE5fJ72B0$XMEo8mlRwfxuT6Q8bDielkRNGIFy.To2qsEYw7hbIm/";
       isNormalUser = true;
       description = "Stefan Bondzulic";
-      extraGroups = [ "wheel" "docker" ];
+      extraGroups = [
+        "wheel"
+        "video"
+        "render"
+        "docker"
+      ];
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF99lU/SLfVoC/Vua9Zbu58d57HfrZZNOZMuI/0xteL openpgp:0x2EED2F74"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL1UcE51oQhUbXEdGHvlJMSmKJQCVsP7xt5Tmj3+m4yN stefanbondzulic@pm.me"
       ];
     };
+
     groups.bondzula.gid = 1000;
   };
 
@@ -54,4 +78,3 @@
 
   system.stateVersion = "24.11";
 }
-
