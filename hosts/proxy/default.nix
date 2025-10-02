a7c24df3c1cb40b709f3dd99558e82735a0140ad
Stefan Bondzulic <stefanbondzulic@gmail.com>
2025-10-02 11:18:48 +0200

    feat: added proxy host

---
+++ hosts/proxy/default.nix
@@ -0,0 +1,51 @@
+{ modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/virtualisation/proxmox-lxc.nix")
+    ../../modules/nixos
+  ];
+
+  networking.hostName = "proxy";
+
+  nix.settings.sandbox = false;
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = false;
+  };
+
+  time.timeZone = "Europe/Belgrade";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users = {
+    users.bondzula = {
+      initialHashedPassword = "$y$j9T$lTYSuKE.0BiJazE5fJ72B0$XMEo8mlRwfxuT6Q8bDielkRNGIFy.To2qsEYw7hbIm/";
+      isNormalUser = true;
+      description = "Stefan Bondzulic";
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF99lU/SLfVoC/Vua9Zbu58d57HfrZZNOZMuI/0xteL openpgp:0x2EED2F74"
+      ];
+    };
+    groups.bondzula.gid = 1000;
+  };
+
+  programs.git.enable = true;
+
+  services.openssh = {
+    enable = true;
+    allowSFTP = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+
+  networking.firewall.enable = false;
+
+  system.stateVersion = "24.11";
+}
+
