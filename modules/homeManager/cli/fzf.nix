d56975123a58b09b12decb07f8532d7597d96e54
Stefan Bondzulic <stefanbondzulic@gmail.com>
2026-07-20 17:39:30 +0200

    fix(cli/fzf): Update changeDirWidget configuration option
    Use the changeDirWidget.command option instead of changeDirWidgetCommand
    to align with current Home Manager configurations.

--- modules/homeManager/cli/fzf.nix
+++ modules/homeManager/cli/fzf.nix
@@ -45,7 +45,7 @@
         "--bind 'ctrl-/:toggle-preview'"
       ];
       defaultCommand = "fd --type f --exclude .git --follow --hidden";
-      changeDirWidgetCommand = "fd --type d --exclude .git --follow --hidden";
+      changeDirWidget.command = "fd --type d --exclude .git --follow --hidden";
     };
   };
 }
